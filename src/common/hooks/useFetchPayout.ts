eefc857ab201ec359e0f2e35c8249cf187de3807
Mizu <1-dimas@hris-system.com>
2024-07-08 02:52:00 +0700

    update

---
+++ src/common/hooks/useFetchPayout.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { PayoutData } from '../../types';
+import { getGamePayout } from '../../services/api/gamePayout';
+
+export const useFetchPayout = () => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState<PayoutData[]>([]);
+
+    useEffect(() => {
+        let ignore = false;
+
+        const fetchPlayerSettings = async () => {
+            try {
+                setLoading(true);
+                const respData = await getGamePayout();
+
+                if (ignore) {
+                    console.log('pyaout', respData);
+
+                    setData(respData);
+                }
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPlayerSettings();
+
+        return () => {
+            ignore = true;
+        };
+    }, []);
+
+    return { loading, data };
+};
--- src/game/components/PayoutContent/index.tsx
+++ src/game/components/PayoutContent/index.tsx
@@ -1,41 +1,11 @@
 import styles from './styles.module.scss';
 import MenuPayoutTop from './MenuPayoutTop';
 import MenuPayoutNote from './MenuPayoutNote';
-import { useEffect, useState } from 'react';
-import { getGamePayout } from '../../../services/api/gamePayout';
-import { PayoutData } from '../../../types';
 import TableMenuPayout from './TableMenuPayout';
+import { useFetchPayout } from '../../../common/hooks/useFetchPayout';
 
 const PayoutContent = () => {
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState<PayoutData[]>([]);
-
-    useEffect(() => {
-        let ignore = false;
-
-        const fetchPlayerSettings = async () => {
-            try {
-                setLoading(true);
-                const respData = await getGamePayout();
-
-                if (ignore) {
-                    console.log('pyaout', respData);
-
-                    setData(respData);
-                }
-            } catch (error) {
-                console.log(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchPlayerSettings();
-
-        return () => {
-            ignore = true;
-        };
-    }, []);
+    const { loading, data } = useFetchPayout();
 
     return (
         <div className={styles.container}>
