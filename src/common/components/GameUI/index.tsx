a730a65fa88bd78abdd45634f166484352d2a097
Mizu <1-dimas@hris-system.com>
2024-07-11 22:40:37 +0700

    update layout

--- src/common/components/GameUI/index.tsx
+++ src/common/components/GameUI/index.tsx
@@ -2,7 +2,7 @@ import MainArea from '../../../game/components/MainArea';
 import LayoutV1 from '../../layouts/v1';
 import LayoutV2 from '../../layouts/v2';
 
-const layout: number = 2;
+const layout: number = 1;
 
 const GameUI = () => {
     if (layout === 1) {
