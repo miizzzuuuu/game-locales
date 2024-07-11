fb590a20a30a9084df3be55d8e180a2af49ce41a
Mizu <1-dimas@hris-system.com>
2024-07-11 11:05:28 +0700

    update

--- src/game/components/TableBet/ButtonBet/index.tsx
+++ src/game/components/TableBet/ButtonBet/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import styles from './styles.module.scss';
 import { useGetChipBet } from '../../../../common/hooks/useGetChipBet';
 import { Bet } from '../../../../types';
-import TableBetChip from '../TableBetChip';
+import TableBetChip from '../../../../common/components/ChipBet';
 import { DisplayHelper } from '../../../../common/utils/DisplayHelper';
 
 interface IProps {
