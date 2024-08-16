ee3177dec0f4706c53258a55c47521fee4610607
Mizu <1-dimas@hris-system.com>
2024-08-16 16:20:00 +0700

    add hook useNewSet

---
+++ src/common/hooks/useNewSet.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useAppSelector } from '../../store/hooks';
+import { selectGameNewSet } from '../../store/slice/gameSlice';
+
+interface Params {
+    handleNewSet: () => void;
+}
+
+export function useNewSet({ handleNewSet }: Params) {
+    const gameNewSet = useAppSelector(selectGameNewSet);
+
+    useEffect(() => {
+        handleNewSet();
+    }, [gameNewSet]);
+}
