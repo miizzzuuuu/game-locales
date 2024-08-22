7cd25358162754babadea41b0a528363e9236147
Mizu <1-dimas@hris-system.com>
2024-08-22 17:33:25 +0700

    add option in component how to play

--- src/common/menus/Items/HowToPlay/Components/index.tsx
+++ src/common/menus/Items/HowToPlay/Components/index.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from 'react';
+import { TOptions } from 'i18next';
 import LabelTranslate from '../../../../components/LabelTranslate';
 import styles from './styles.module.scss';
 
 interface IProps {
     keyLang?: string;
     value: string;
+    option?: TOptions;
     className?: string;
 }
 
@@ -24,23 +26,25 @@ export const Container = ({ children, className }: ContainerProps) => {
     );
 };
 
-export const Heading2 = ({ keyLang, className, value }: IProps) => {
+export const Heading2 = ({ keyLang, className, value, option }: IProps) => {
     return (
         <LabelTranslate
             type="h2"
             className={`${styles['heading-2']}${className ? ` ${className}` : ''}`}
             keyLang={keyLang}
             value={value}
+            option={option}
         />
     );
 };
 
-export const P = ({ keyLang, className, value }: IProps) => {
+export const P = ({ keyLang, className, value, option }: IProps) => {
     return (
         <LabelTranslate
             keyLang={keyLang}
             className={`${styles['text-htp']}${className ? ` ${className}` : ''}`}
             value={value}
+            option={option}
         />
     );
 };
