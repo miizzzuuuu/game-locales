e4c554e76627aadd3e902137e0c2eefe94846919
Mizu <1-dimas@hris-system.com>
2024-08-22 15:08:46 +0700

    add component in how to play

--- src/common/menus/Items/HowToPlay/Components/index.tsx
+++ src/common/menus/Items/HowToPlay/Components/index.tsx
@@ -1,15 +1,41 @@
+import { ReactNode } from 'react';
 import LabelTranslate from '../../../../components/LabelTranslate';
 import styles from './styles.module.scss';
 
 interface IProps {
     keyLang?: string;
     value: string;
+    className?: string;
 }
 
-const Heading2 = ({ keyLang, value }: IProps) => {
+interface UlProps {
+    children?: ReactNode;
+    className?: string;
+}
+
+export const Heading2 = ({ keyLang, className, value }: IProps) => {
     return (
-        <LabelTranslate type="h2" className={styles['heading-2']} keyLang={keyLang} value={value} />
+        <LabelTranslate
+            type="h2"
+            className={`${styles['heading-2']}${className ? ` ${className}` : ''}`}
+            keyLang={keyLang}
+            value={value}
+        />
     );
 };
 
-export default Heading2;
+export const P = ({ keyLang, className, value }: IProps) => {
+    return (
+        <LabelTranslate
+            keyLang={keyLang}
+            className={`${styles['text-htp']}${className ? ` ${className}` : ''}`}
+            value={value}
+        />
+    );
+};
+
+export const Ul = ({ className, children }: UlProps) => {
+    return (
+        <ul className={`${styles['htp-list']}${className ? `${className}` : ''}`}>{children}</ul>
+    );
+};
