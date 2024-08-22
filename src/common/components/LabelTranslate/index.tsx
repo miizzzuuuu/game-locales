cb7cb5dbfc7ba3672944e7c468640502c8efa186
Mizu <1-dimas@hris-system.com>
2024-08-22 14:42:40 +0700

    update LabelTranslate

--- src/common/components/LabelTranslate/index.tsx
+++ src/common/components/LabelTranslate/index.tsx
@@ -1,23 +1,26 @@
 import { TOptions } from 'i18next';
 import { useAppTranslate } from '../../../services/i18next/hooks';
+import { createElement, ComponentType } from 'react';
 
 interface IProps {
+    type?: string | ComponentType<any>;
     value: string;
     keyLang?: string;
     option?: TOptions;
-
     className?: string;
     style?: React.CSSProperties;
 }
 
-function LabelTranslate({ keyLang, value, option, className, style }: IProps) {
+function LabelTranslate({ type = 'p', keyLang, value, option, className, style }: IProps) {
     const { t } = useAppTranslate(keyLang);
 
-    return (
-        <p className={className} style={style}>
-            {t(value, option)}
-        </p>
-    );
+    const props = { className, style };
+
+    if (typeof type === 'string') {
+        return createElement(type, props, t(value, option));
+    }
+
+    return createElement(type, { ...props }, t(value, option));
 }
 
 export default LabelTranslate;
