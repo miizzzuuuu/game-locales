95a3c9122f22be86a92169477dcfd3639f2c0462
Mizu <1-dimas@hris-system.com>
2024-08-27 17:53:03 +0700

    update placing bet

--- src/game/components/HowToPlayContent/Items/PlacingABet/index.tsx
+++ src/game/components/HowToPlayContent/Items/PlacingABet/index.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import LabelTranslate from '../../../../../common/components/LabelTranslate';
 import { Container, Heading2 } from '../../../../../common/menus/Items/HowToPlay/Components';
 import HowToPlayCard from '../../../../../common/menus/Items/HowToPlay/HowToPlayCard';
@@ -8,35 +9,44 @@ import SVGCorner from './SVG/SVGCorner';
 import SVGDouble from './SVG/SVGDouble';
 import SVGSingle from './SVG/SVGSingle';
 
+type Item = { key: string; icon?: ReactNode };
+
+const data: Item[] = [
+    {
+        key: 'single-bet',
+        icon: <SVGSingle />,
+    },
+    {
+        key: 'split-bet',
+        icon: <SVGDouble />,
+    },
+    {
+        key: 'corner-bet',
+        icon: <SVGCorner />,
+    },
+    {
+        key: 'row-column-bets',
+        icon: <SVGColRow />,
+    },
+    {
+        key: '50-50-bet',
+        icon: <SVGBet50 />,
+    },
+];
+
 const PlacingABet = () => {
     const keyLang = 'htp.placing-a-bet';
 
     return (
         <HowToPlayCard title={<LabelTranslate value="title" keyLang={keyLang} />}>
             <Container>
-                <Heading2 keyLang={keyLang} value="single-bet" />
                 <div>
-                    <SVGSingle />
-                </div>
-
-                <Heading2 keyLang={keyLang} value="split-bet" />
-                <div>
-                    <SVGDouble />
-                </div>
-
-                <Heading2 keyLang={keyLang} value="corner-bet" />
-                <div>
-                    <SVGCorner />
-                </div>
-
-                <Heading2 keyLang={keyLang} value="row-column-bets" />
-                <div>
-                    <SVGColRow />
-                </div>
-
-                <Heading2 keyLang={keyLang} value="50-50-bet" />
-                <div>
-                    <SVGBet50 />
+                    {data.map((value, idx) => (
+                        <div key={idx}>
+                            <Heading2 keyLang={keyLang} value={value.key} />
+                            <div>{value.icon}</div>
+                        </div>
+                    ))}
                 </div>
             </Container>
         </HowToPlayCard>
