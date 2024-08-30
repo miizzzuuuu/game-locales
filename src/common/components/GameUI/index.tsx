e1cf22bae5d4cd97d9e926591ba39c910480ddea
Mizu <1-dimas@hris-system.com>
2024-08-31 01:40:48 +0700

    fix layout version

--- src/common/components/GameUI/index.tsx
+++ src/common/components/GameUI/index.tsx
@@ -1,10 +1,10 @@
 import { useAppSelector } from '../../../store/hooks';
-import { selectShowMiniHowToPlay } from '../../../store/slice/gameStateSlice';
+import { selectLayoutVersion } from '../../../store/slice/gameStateSlice';
 import LayoutV1 from '../../layouts/v1';
 import LayoutV2 from '../../layouts/v2';
 
 const GameUI = () => {
-    const layoutVersion = useAppSelector(selectShowMiniHowToPlay);
+    const layoutVersion = useAppSelector(selectLayoutVersion);
 
     if (layoutVersion === 1) {
         return <LayoutV1></LayoutV1>;
