0f702c399e1f8c4e887740fac38be43329fe9b15
Mizu <1-dimas@hris-system.com>
2024-09-11 14:47:36 +0700

    add payout

--- src/api/server/db/payouts.ts
+++ src/api/server/db/payouts.ts
@@ -1,6 +1,89 @@
 import { PayoutData } from '../../../types';
 
 export const payouts: Record<string, PayoutData[]> = {
+    p6: [
+        {
+            name: 'one number',
+            payout: 18,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'two numbers',
+            payout: 8.5,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'four numbers',
+            payout: 3.75,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'single row',
+            payout: 2.16,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'single column',
+            payout: 3.75,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'double row',
+            payout: 0.95,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'double column',
+            payout: 1.37,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'side bet',
+            payout: null,
+            items: [
+                {
+                    name: 'big',
+                    payout: 0.95,
+                },
+                {
+                    name: 'small',
+                    payout: 0.95,
+                },
+                {
+                    name: 'odd',
+                    payout: 0.95,
+                },
+                {
+                    name: 'even',
+                    payout: 0.95,
+                },
+                {
+                    name: 'red',
+                    payout: 0.95,
+                },
+                {
+                    name: 'black',
+                    payout: 0.95,
+                },
+            ],
+            min: 5000,
+            max: 5000000,
+        },
+    ],
     p6b: [
         {
             name: 'one number',
@@ -78,6 +161,186 @@ export const payouts: Record<string, PayoutData[]> = {
                 {
                     name: 'black',
                     payout: 0.95,
+                },
+            ],
+            min: 5000,
+            max: 5000000,
+        },
+    ],
+    p7d: [
+        {
+            name: 'single',
+            payout: 35,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'split',
+            payout: 17,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'street',
+            payout: 11,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'corner',
+            payout: 8,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'six line / double street',
+            payout: 5,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'trio',
+            payout: 11,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'dozen',
+            payout: 2,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'column',
+            payout: 2,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'side bet',
+            payout: null,
+            items: [
+                {
+                    name: '1-18',
+                    payout: 1,
+                },
+                {
+                    name: '19-36',
+                    payout: 1,
+                },
+                {
+                    name: 'odd',
+                    payout: 1,
+                },
+                {
+                    name: 'even',
+                    payout: 1,
+                },
+                {
+                    name: 'red',
+                    payout: 1,
+                },
+                {
+                    name: 'black',
+                    payout: 1,
+                },
+            ],
+            min: 5000,
+            max: 5000000,
+        },
+    ],
+    p7g: [
+        {
+            name: 'single',
+            payout: 35,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'split',
+            payout: 17,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'street',
+            payout: 11,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'corner',
+            payout: 8,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'six line / double street',
+            payout: 5,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'trio',
+            payout: 11,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'dozen',
+            payout: 2,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'column',
+            payout: 2,
+            items: [],
+            min: 1000,
+            max: 1000000,
+        },
+        {
+            name: 'side bet',
+            payout: null,
+            items: [
+                {
+                    name: '1-18',
+                    payout: 1,
+                },
+                {
+                    name: '19-36',
+                    payout: 1,
+                },
+                {
+                    name: 'odd',
+                    payout: 1,
+                },
+                {
+                    name: 'even',
+                    payout: 1,
+                },
+                {
+                    name: 'red',
+                    payout: 1,
+                },
+                {
+                    name: 'black',
+                    payout: 1,
                 },
             ],
             min: 5000,
