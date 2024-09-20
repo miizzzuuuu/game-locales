a848696d509f3b9b4f70ea41d4aee596226cd002
Mizu <1-dimas@hris-system.com>
2024-09-20 12:31:11 +0700

    update chip image

--- src/common/components/ChipBet/index.tsx
+++ src/common/components/ChipBet/index.tsx
@@ -4,9 +4,9 @@ import styles from './styles.module.scss';
 import { StringHelper } from '../../utils/StringHelper';
 import { useAppSelector } from '../../../store/hooks';
 import { selectShowChip } from '../../../store/slice/gameStateSlice';
-import SVGChip from '../SVG/SVGChip';
 import { selectChipBase } from '../../../store/slice/chipSlice';
 import { ChipHelper } from '../../utils/ChipHelper';
+import SVGChip from '../SVG/SVGChip';
 
 interface IProps {
     value: number;
@@ -17,12 +17,18 @@ const ChipBet = ({ value, ignoreTransparent }: IProps) => {
     const stringValue = StringHelper.formatChipText(value);
 
     const chipBase = useAppSelector(selectChipBase);
+    // const image = ChipHelper.getChipImageByAmount(value, chipBase);
     const color = ChipHelper.getChipColorByAmount(value, chipBase);
 
     const showChip = useAppSelector(selectShowChip) || ignoreTransparent;
 
     return (
         <div className={`${styles['chip']}${showChip ? '' : ` ${styles.show}`}`}>
+            {/* <img className={styles.bg} src={image} />
+
+            <div className={styles.text} data-value={stringValue}>
+                {stringValue}
+            </div> */}
             <SVGChip color={color} value={stringValue} />
 
             <div className={styles['chip-light']} />
--- src/common/utils/ChipHelper.ts
+++ src/common/utils/ChipHelper.ts
@@ -1,3 +1,14 @@
+import CHIP_1 from '../../assets/img/chips/chip-1.svg';
+import CHIP_2 from '../../assets/img/chips/chip-2.svg';
+import CHIP_3 from '../../assets/img/chips/chip-3.svg';
+import CHIP_4 from '../../assets/img/chips/chip-4.svg';
+import CHIP_5 from '../../assets/img/chips/chip-5.svg';
+import CHIP_6 from '../../assets/img/chips/chip-6.svg';
+import CHIP_7 from '../../assets/img/chips/chip-7.svg';
+import CHIP_8 from '../../assets/img/chips/chip-8.svg';
+import CHIP_9 from '../../assets/img/chips/chip-9.svg';
+import CHIP_10 from '../../assets/img/chips/chip-10.svg';
+
 export class ChipHelper {
     static chipColors = [
         '#8B8B8B',
@@ -10,6 +21,19 @@ export class ChipHelper {
         '#45BCD6',
         '#FF8319',
         '#FF48B6',
+    ];
+
+    static CHIP_IMAGES = [
+        CHIP_1,
+        CHIP_2,
+        CHIP_3,
+        CHIP_4,
+        CHIP_5,
+        CHIP_6,
+        CHIP_7,
+        CHIP_8,
+        CHIP_9,
+        CHIP_10,
     ];
 
     public static getChipColorByIndex(index: number): string {
@@ -32,5 +56,23 @@ export class ChipHelper {
         }
 
         return this.chipColors[index];
+    }
+
+    static getChipImageByAmount(amount: number, chipBase: number[]): string {
+        if (chipBase.length === 0) return this.CHIP_IMAGES[0];
+
+        const n = this.CHIP_IMAGES.length;
+        let index: number = n - 1;
+
+        if (amount >= chipBase[index]) return this.CHIP_IMAGES[index];
+
+        for (let i = 1; i < chipBase.length; i++) {
+            if (amount < chipBase[i] && i <= n) {
+                index = i - 1;
+                break;
+            }
+        }
+
+        return this.CHIP_IMAGES[index];
     }
 }
