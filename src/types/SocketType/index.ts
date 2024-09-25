5636292ebcab116e66aa0c341566b548bb6fe6c8
Mizu <1-dimas@hris-system.com>
2024-09-25 22:56:38 +0700

    update type load new value

--- src/types/SocketType/index.ts
+++ src/types/SocketType/index.ts
@@ -14,7 +14,7 @@ export type LoadNewValueData = {
     game: string; // "game": "6dcolor",
     win: string; // "win": "1r,2b,3y",
     pcode: string; // "pcode":"m43",
-    timer: string; // "timer":"30",
+    timer: string | number; // "timer":"30",
     periode: number; // "periode":7011
     shoePeriode?: string;
 };
