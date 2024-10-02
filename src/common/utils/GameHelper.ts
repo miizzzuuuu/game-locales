640fbaf0bc95f2696735ecf025a6e7c0b1e5d23c
Mizu <1-dimas@hris-system.com>
2024-10-03 03:52:51 +0700

    update regex

--- src/common/utils/GameHelper.ts
+++ src/common/utils/GameHelper.ts
@@ -176,21 +176,21 @@ export const getEventNewSet = () => {
 
     const variant = getVariant();
 
-    if (/^m22/.test(_pcode)) {
+    if (_pcode.startsWith('m22')) {
         baseNewSet = 'baccaratNewSet';
-    } else if (/^m23/.test(_pcode)) {
+    } else if (_pcode.startsWith('m23')) {
         baseNewSet = 'dragonTigerNewSet';
-    } else if (/^m24/.test(_pcode)) {
+    } else if (_pcode.startsWith('m24')) {
         baseNewSet = 'niuniuNewSet';
-    } else if (/^m27/.test(_pcode)) {
+    } else if (_pcode.startsWith('m27')) {
         baseNewSet = 'shioFightNewSet';
-    } else if (/^m28/.test(_pcode)) {
+    } else if (_pcode.startsWith('m28')) {
         baseNewSet = 'idn4standNewSet';
-    } else if (/^m38/.test(_pcode)) {
+    } else if (_pcode.startsWith('m38')) {
         baseNewSet = 'baccaratmachineNewSet';
-    } else if (/^m41/.test(_pcode)) {
+    } else if (_pcode.startsWith('m41')) {
         baseNewSet = 'dominoNewSet';
-    } else if (/^m46/.test(_pcode)) {
+    } else if (_pcode.startsWith('m46')) {
         baseNewSet = 'cemeNewSet';
     } else {
         return baseNewSet;
