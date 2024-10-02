7913a385acaebcb8d5cb29c052405da46287b6ba
Mizu <1-dimas@hris-system.com>
2024-10-02 14:53:14 +0700

    fix modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     return {
         plugins: [react()],
+        css: {
+            postcss: {
+                plugins: [autoprefixer({})],
+            },
+        },
         build: {
             chunkSizeWarningLimit: 1600,
             rollupOptions: {
