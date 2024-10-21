d648660ea9aa5be6e2ee66e26d2d675bbb616a70
Mizu <1-dimas@hris-system.com>
2024-10-21 14:12:06 +0700

    update streaming

--- src/common/components/Streaming/index.tsx
+++ src/common/components/Streaming/index.tsx
@@ -1,3 +1,4 @@
+import { DisplayHelper } from '../../utils/DisplayHelper';
 import { Features } from '../../utils/Features';
 import Video from '../Video';
 import { useGetStreamingSize } from './hooks/useGetStreamingSize';
@@ -5,15 +6,19 @@ import { useGetStreamingSize } from './hooks/useGetStreamingSize';
 import styles from './styles.module.scss';
 
 const Streaming = () => {
+    const orientation = DisplayHelper.getOrientation();
+
     useGetStreamingSize();
 
     return (
         <div
             className={`${styles.streaming} ${Features.STREAMING_LANDSCAPE_LETTER_BOX ? styles['letter-box'] : styles['non-letter-box']}`}
         >
+            {orientation === 'landscape' && <div className={styles.left}></div>}
             <div className={styles['video-container']}>
                 <Video />
             </div>
+            {orientation === 'landscape' && <div className={styles.right}></div>}
 
             <div className={styles['video-background']}></div>
         </div>
