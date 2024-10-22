4dbcb52c8ffd70676d5c391402ab854b4433ac9a
Mizu <1-dimas@hris-system.com>
2024-10-22 18:23:39 +0700

    update format text transaction

--- src/game/components/BettingItemTransaction/index.tsx
+++ src/game/components/BettingItemTransaction/index.tsx
@@ -1,9 +1,26 @@
+import { memo } from 'react';
+
 interface IProps {
     data: string;
 }
 
 const BettingItemTransaction = ({ data }: IProps) => {
-    return data;
+    const textData = () => {
+        const indexWild = data.indexOf('wild');
+        if (indexWild >= 0) {
+            return data.slice(0, indexWild) + ' wild';
+        }
+
+        const indexPair = data.indexOf('pair');
+        if (indexPair >= 0) {
+            return data.slice(0, indexPair) + ' pair';
+        }
+
+        return data;
+    };
+
+    return <span style={{ textTransform: 'capitalize' }}>{textData()}</span>;
 };
 
-export default BettingItemTransaction;
+const MemoizedMemoizeBettingItemTransaction = memo(BettingItemTransaction);
+export default MemoizedMemoizeBettingItemTransaction;
