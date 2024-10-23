9ea43475d6b09b6473def528eff5a76f5e89b56e
Mizu <1-dimas@hris-system.com>
2024-10-23 14:56:17 +0700

    update streaming

--- src/common/components/Streaming/index.tsx
+++ src/common/components/Streaming/index.tsx
@@ -5,20 +5,30 @@ import { useGetStreamingSize } from './hooks/useGetStreamingSize';
 
 import styles from './styles.module.scss';
 
+const streamingClasses = `${styles.streaming} ${Features.STREAMING_LANDSCAPE_LETTER_BOX ? styles['letter-box'] : styles['non-letter-box']}`;
+
 const Streaming = () => {
     const orientation = DisplayHelper.getOrientation();
 
     useGetStreamingSize();
 
     return (
-        <div
-            className={`${styles.streaming} ${Features.STREAMING_LANDSCAPE_LETTER_BOX ? styles['letter-box'] : styles['non-letter-box']}`}
-        >
-            {orientation === 'landscape' && <div className={styles.left}></div>}
-            <div className={styles['video-container']}>
-                <Video />
-            </div>
-            {orientation === 'landscape' && <div className={styles.right}></div>}
+        <div className={streamingClasses}>
+            {orientation === 'landscape' ? (
+                <>
+                    <div className={styles.left} />
+                    <div className={styles['video-container']}>
+                        <div className={styles['video-left']} />
+                        <Video />
+                        <div className={styles['video-right']} />
+                    </div>
+                    <div className={styles.right} />
+                </>
+            ) : (
+                <div className={styles['video-container']}>
+                    <Video />
+                </div>
+            )}
 
             <div className={styles['video-background']}></div>
         </div>
