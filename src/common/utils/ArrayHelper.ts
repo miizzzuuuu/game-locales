b81c1049c6e252afb21a15a40ed959139f9db7a3
Mizu <1-dimas@hris-system.com>
2024-10-23 16:51:23 +0700

    tranform ArrayHelper to function

--- src/common/utils/ArrayHelper.ts
+++ src/common/utils/ArrayHelper.ts
@@ -1,23 +1,26 @@
-export class ArrayHelper {
-    static range = (size: number, startAt = 0): number[] => {
-        return [...Array(size).keys()].map((i) => i + startAt);
-    };
+export function range(size: number, startAt = 0): number[] {
+    return [...Array(size).keys()].map((i) => i + startAt);
+}
 
-    static newArray2D = (col: number, row: number, fill: any = null) => {
-        return Array(row)
-            .fill(undefined)
-            .map(() => Array(col).fill(fill));
-    };
+export function newArray(length: number, fill: any = null) {
+    return Array(length).fill(fill);
+}
 
-    static newArray = (length: number, fill: any = null) => {
-        return Array(length).fill(fill);
-    };
+export function newArray2D(col: number, row: number, fill: any = null) {
+    return Array(row)
+        .fill(undefined)
+        .map(() => Array(col).fill(fill));
+}
 
-    static takeLeft = <T>(array: T[], n = 1): T[] => {
-        return n >= array.length ? [...array] : array.slice(0, n);
-    };
+export function takeLeft<T>(array: T[], n = 1): T[] {
+    return n >= array.length ? [...array] : array.slice(0, n);
+}
 
-    static takeRight = <T>(array: T[], n = 1): T[] => {
-        return n >= array.length ? [...array] : array.slice(array.length - n, array.length);
-    };
+export function takeRight<T>(array: T[], n = 1): T[] {
+    return n >= array.length ? [...array] : array.slice(array.length - n, array.length);
+}
+
+export function hasCommonElement<T>(array1: T[], array2: T[]): boolean {
+    const commonElements = array1.filter((item) => array2.includes(item));
+    return commonElements.length > 0;
 }
--- src/game/components/HowToPlayContent/Items/Rules/index.tsx
+++ src/game/components/HowToPlayContent/Items/Rules/index.tsx
@@ -1,7 +1,7 @@
 import LabelTranslate from '../../../../../common/components/LabelTranslate';
 import { Container, P, Ul } from '../../../../../common/menus/Items/HowToPlay/Components';
 import HowToPlayCard from '../../../../../common/menus/Items/HowToPlay/HowToPlayCard';
-import { ArrayHelper } from '../../../../../common/utils/ArrayHelper';
+import { range } from '../../../../../common/utils/ArrayHelper';
 
 const Rules = () => {
     const keyLang = 'htp.rules';
@@ -10,7 +10,7 @@ const Rules = () => {
         <HowToPlayCard title={<LabelTranslate value="title" keyLang={keyLang} />}>
             <Container>
                 <Ul>
-                    {ArrayHelper.range(6).map((_, idx) => (
+                    {range(6).map((_, idx) => (
                         <li key={idx}>
                             <P keyLang={keyLang} value={`steps.${idx}`} dangerouslySetInnerHTML />
                         </li>
