60a035581e1774172d00ec33e92cb2d5daf32527
Mizu <1-dimas@hris-system.com>
2024-10-24 16:09:12 +0700

    update payout menu

--- src/common/menus/Items/Payout/PayoutContent/index.tsx
+++ src/common/menus/Items/Payout/PayoutContent/index.tsx
@@ -3,6 +3,7 @@ import MenuPayoutTop from './MenuPayoutTop';
 import TableMenuPayout from './TableMenuPayout';
 import { useFetchPayout } from '../../../../hooks/useFetchPayout';
 import Loading from '../../../../components/Loading';
+import PayoutNote from './PayoutNote';
 
 const PayoutContent = () => {
     const { loading, data } = useFetchPayout();
@@ -12,7 +13,14 @@ const PayoutContent = () => {
             <MenuPayoutTop />
 
             <div className={styles['table-wrapper']}>
-                {loading ? <Loading /> : <TableMenuPayout data={data} />}
+                {loading ? (
+                    <Loading />
+                ) : (
+                    <>
+                        <TableMenuPayout data={data} />
+                        <PayoutNote content="payout-no-initial-bet" />
+                    </>
+                )}
             </div>
         </div>
     );
