a199a5e61cf6849fe59dc2b2d2a1222b015fe6c3
Mizu <1-dimas@hris-system.com>
2025-01-30 23:37:13 +0700

    add button function for promotion

--- src/common/desktop/TopRight/index.tsx
+++ src/common/desktop/TopRight/index.tsx
@@ -1,16 +1,20 @@
+import { useAppSelector } from '../../../store/hooks';
+import { selectEventIdnliveList } from '../../../store/slice/eventIdnliveSlice';
 import ButtonFullscreen from '../ButtonFullscreen';
 import ButtonHTP from '../ButtonHTP';
-// import ButtonPromotion from '../ButtonPromotion';
+import ButtonPromotion from '../ButtonPromotion';
 import ButtonSettings from '../ButtonSettings';
 import ButtonTransactions from '../ButtonTransactions';
 import styles from './styles.module.scss';
 
 const TopRight = () => {
+    const eventsIdnliveList = useAppSelector(selectEventIdnliveList);
+
     return (
         <div className={styles.container}>
             <ButtonTransactions />
             <ButtonHTP />
-            {/* <ButtonPromotion /> */}
+            {eventsIdnliveList.length > 0 && <ButtonPromotion />}
             <ButtonSettings />
             <ButtonFullscreen />
         </div>
