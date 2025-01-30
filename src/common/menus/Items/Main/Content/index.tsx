2ec6c4430aa34f75286306831bbf249d87aaafd9
Mizu <1-dimas@hris-system.com>
2025-01-30 23:33:50 +0700

    active menu promotion

--- src/common/menus/Items/Main/Content/index.tsx
+++ src/common/menus/Items/Main/Content/index.tsx
@@ -1,22 +1,26 @@
-import { useAppDispatch } from '../../../../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../../../../store/hooks';
+import { selectEventIdnliveList } from '../../../../../store/slice/eventIdnliveSlice';
 import {
     toggleMenuHTP,
     toggleMenuHistory,
+    toggleMenuMain,
     toggleMenuPayout,
     toggleMenuSettings,
 } from '../../../../../store/slice/menuSlice';
+import { sendMessageToParent } from '../../../../utils/FunctionHelper';
 import BackToLobby from '../BackToLobby';
 import Item from '../Item';
 import SVGIconHistory from '../SVG/SVGIconHistory';
 import SVGIconHowToPlay from '../SVG/SVGIconHowToPlay';
 import SVGIconPayout from '../SVG/SVGIconPayout';
-// import SVGIconPromotion from '../SVG/SVGIconPromotion';
+import SVGIconPromotion from '../SVG/SVGIconPromotion';
 import SVGIconSettings from '../SVG/SVGIconSettings';
-
 import styles from './styles.module.scss';
 
 const Content = () => {
     const dispatch = useAppDispatch();
+
+    const eventsIdnliveList = useAppSelector(selectEventIdnliveList);
 
     return (
         <div className={styles.main}>
@@ -37,7 +41,21 @@ const Content = () => {
                 text="payout-and-limit"
                 onClick={() => dispatch(toggleMenuPayout())}
             />
-            {/* <Item icon={<SVGIconPromotion />} text="promotion" /> */}
+            {eventsIdnliveList.length > 0 && (
+                <Item
+                    icon={<SVGIconPromotion />}
+                    text="promotion"
+                    onClick={() => {
+                        sendMessageToParent({
+                            source: 'LIVE_GAME',
+                            type: 'OPEN_MODAL_EVENT',
+                            payload: eventsIdnliveList[0],
+                        });
+
+                        dispatch(toggleMenuMain());
+                    }}
+                />
+            )}
             <Item
                 icon={<SVGIconSettings />}
                 text="settings"
