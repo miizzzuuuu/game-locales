a805b4c46dfba1e9b685e23ac0ba996d9b8a23fa
Mizu <1-dimas@hris-system.com>
2025-02-05 08:54:35 +0700

    update version

--- src/common/components/Version/index.tsx
+++ src/common/components/Version/index.tsx
@@ -1,6 +1,6 @@
 import styles from './styles.module.scss';
 
-const version = '1.0.1';
+const version = import.meta.env.VITE_APP_VERSION;
 
 const Version = () => {
     return <span className={styles.version}>v{version}</span>;
