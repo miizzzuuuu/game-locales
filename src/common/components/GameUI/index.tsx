f653b90885cca0f3c05198bfcf530a4a07b66dfd
Mizu <1-dimas@hris-system.com>
2025-02-25 13:44:35 +0700

    dynamic button

--- src/common/components/GameUI/index.tsx
+++ src/common/components/GameUI/index.tsx
@@ -1,4 +1,4 @@
-// import MainArea from '../../../game/components/MainArea';
+import MainArea from '../../../game/components/MainArea';
 import MainAreaDesktop from '../../../game/components/MainAreaDesktop';
 import { useAppSelector } from '../../../store/hooks';
 import { selectDevice } from '../../../store/slice/windowSlice';
@@ -8,32 +8,32 @@ import LayoutV2 from '../../layouts/v2';
 import LayoutV3 from '../../layouts/v3';
 import { FEATURES } from '../../utils/Features';
 
-const layoutVersion = FEATURES.LAYOUT_VERSION;
-const Layout =
-    layoutVersion === 1
-        ? LayoutV1
-        : layoutVersion === 2
-          ? LayoutV2
-          : layoutVersion === 3
-            ? LayoutV3
-            : null;
+const LAYOUT_COMPONENTS = {
+    1: LayoutV1,
+    2: LayoutV2,
+    3: LayoutV3,
+} as const;
+
+const Layout = LAYOUT_COMPONENTS[FEATURES.LAYOUT_VERSION] ?? LayoutV1;
+
+const DesktopGame = () => (
+    <GameDesktop>
+        <MainAreaDesktop />
+    </GameDesktop>
+);
 
 const GameUI = () => {
     const device = useAppSelector(selectDevice);
 
     if (device === 'desktop') {
-        return (
-            <GameDesktop>
-                <MainAreaDesktop />
-            </GameDesktop>
-        );
+        return <DesktopGame />;
     }
 
-    if (!Layout) {
-        return null;
-    }
-
-    return <Layout>{/* <MainArea /> */}</Layout>;
+    return (
+        <Layout>
+            <MainArea />
+        </Layout>
+    );
 };
 
 export default GameUI;
