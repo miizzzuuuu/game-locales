23d22f34e04542b3109717d673b4fe34c7338164
Mizu <1-dimas@hris-system.com>
2025-03-02 23:43:48 +0700

    refactor dragon tiger

--- src/game/components/TableBetV2/index.tsx
+++ src/game/components/TableBetV2/index.tsx
@@ -12,7 +12,8 @@ import BetTie from './ButtonBet/BetTie';
 import BetTiger from './ButtonBet/BetTiger';
 import BetTigerPair from './ButtonBet/BetTigerPair';
 import BetTigerWild from './ButtonBet/BetTigerWild';
-import styles from './index.module.scss';
+import Ornament from './Ornament';
+import styles from './styles.module.scss';
 
 const TableBetV2 = () => {
     const betIsOpen = useAppSelector(selectBetIsOpen);
@@ -89,29 +90,7 @@ const TableBetV2 = () => {
                 </div>
             </div>
 
-            <div className={`${styles.stick} ${styles.left}`}>
-                <div className={styles.long}></div>
-                <div className={styles['stick-top']}></div>
-                <div className={styles['stick-bottom']}></div>
-                <div className={styles['top-ornamen']}>
-                    <div className={styles['top-ornamen-circle']}></div>
-                </div>
-                <div className={styles['bottom-ornamen']}>
-                    <div className={styles['bottom-ornamen-circle']}></div>
-                </div>
-            </div>
-
-            <div className={`${styles.stick} ${styles.right}`}>
-                <div className={`${styles.long}`}></div>
-                <div className={`${styles['stick-top']}`}></div>
-                <div className={`${styles['stick-bottom']}`}></div>
-                <div className={`${styles['top-ornamen']}`}>
-                    <div className={`${styles['top-ornamen-circle']}`}></div>
-                </div>
-                <div className={`${styles['bottom-ornamen']}`}>
-                    <div className={`${styles['bottom-ornamen-circle']}`}></div>
-                </div>
-            </div>
+            <Ornament />
         </div>
     );
 };
