c920b13c074d5c2dcc10e4a79de493e503709dc9
Mizu <1-dimas@hris-system.com>
2025-03-03 00:05:09 +0700

    update html

--- src/common/components/HowToPlay/ui/P/index.tsx
+++ src/common/components/HowToPlay/ui/P/index.tsx
@@ -1,7 +1,20 @@
-import { PropsWithChildren } from 'react';
+import { ReactNode } from 'react';
 import styles from './styles.module.scss';
 
-const P = ({ children }: PropsWithChildren) => {
+type IProps = {
+    children: ReactNode;
+};
+
+const containsHtml = (str: string): boolean => {
+    const htmlRegex = /<[^>]+>/;
+    return htmlRegex.test(str);
+};
+
+const P = ({ children }: IProps) => {
+    if (typeof children === 'string' && containsHtml(children)) {
+        return <p className={styles.p} dangerouslySetInnerHTML={{ __html: children }} />;
+    }
+
     return <p className={styles.p}>{children}</p>;
 };
 
