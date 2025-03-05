9d912b6dad9f9801bd519c53e2e9c63be8f32790
Mizu <1-dimas@hris-system.com>
2025-03-05 19:08:06 +0700

    update styles P

--- src/common/components/HowToPlay/ui/P/index.tsx
+++ src/common/components/HowToPlay/ui/P/index.tsx
@@ -1,9 +1,10 @@
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 import styles from './styles.module.scss';
 
 type IProps = {
     children?: ReactNode;
     className?: string;
+    style?: CSSProperties;
 };
 
 const containsHtml = (str: string): boolean => {
@@ -11,17 +12,22 @@ const containsHtml = (str: string): boolean => {
     return htmlRegex.test(str);
 };
 
-const P = ({ children, className }: IProps) => {
+const P = ({ children, className, style }: IProps) => {
     if (typeof children === 'string' && containsHtml(children)) {
         return (
             <p
                 className={`${styles.p}${className ? ` ${className}` : ''}`}
+                style={style}
                 dangerouslySetInnerHTML={{ __html: children }}
             />
         );
     }
 
-    return <p className={`${styles.p}${className ? ` ${className}` : ''}`}>{children}</p>;
+    return (
+        <p className={`${styles.p}${className ? ` ${className}` : ''}`} style={style}>
+            {children}
+        </p>
+    );
 };
 
 export default P;
